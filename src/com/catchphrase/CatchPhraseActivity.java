f19fcb2646f9491e913d5f08f84212d90c4eaba4
hnz-evn <hnz.evn@gmail.com>
2014-10-25 14:03:04 -0500

    Initial commit
    Re-built project as a basic Android app.

---
+++ src/com/catchphrase/CatchPhraseActivity.java
@@ -0,0 +1,15 @@
+package com.catchphrase;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CatchPhraseActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_catch_phrase);
+	}
+}
