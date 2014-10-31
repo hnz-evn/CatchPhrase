e536795ace0d7da02e430eb718e8a04b5cde142e
hnz-evn <hnz.evn@gmail.com>
2014-10-30 22:40:28 -0500

    Added timer

--- src/com/catchphrase/CatchPhraseActivity.java
+++ src/com/catchphrase/CatchPhraseActivity.java
@@ -2,33 +2,53 @@ package com.catchphrase;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CatchPhraseActivity extends Activity {
 
 	private Button mNextButton;
 	private Button mSkipButton;
 	private TextView mCatchphraseTextView;
-	private Words words;
+	private TextView mTimerTextView;
+	private Words mWords;
+	private CountDownTimer mTimer;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_catch_phrase);
-		words = new Words();
 		
+		mWords = new Words();
+		
+		// put first word into phrase textview
 		mCatchphraseTextView = (TextView) findViewById(R.id.catchphrase_display);
-		mCatchphraseTextView.setText(words.getNextWord());
+		mCatchphraseTextView.setText(mWords.getNextWord());
+		
+		// set up timer and tie it to timer textview
+		mTimerTextView = (TextView) findViewById(R.id.timer);
+		mTimer = new CountDownTimer(15000, 1000) {
+			public void onTick(long millisUntilFinished) {
+				mTimerTextView.setText(String.valueOf(millisUntilFinished / 1000) + "s");
+			}
+			public void onFinish() {
+				mTimerTextView.setText("0s");
+				Toast toast = Toast.makeText(getApplicationContext(), "Time's up!", Toast.LENGTH_LONG);
+				toast.show();
+			}
+		};
+		mTimer.start();
 		
 		// initialize buttons and set onclick events
 		mNextButton = (Button) findViewById(R.id.next_button);
 		mNextButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				mCatchphraseTextView.setText(words.getNextWord());
+				mCatchphraseTextView.setText(mWords.getNextWord());
 			}
 		});
 		
@@ -36,7 +56,7 @@ public class CatchPhraseActivity extends Activity {
 		mSkipButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				mCatchphraseTextView.setText(words.getNextWord());
+				mCatchphraseTextView.setText(mWords.getNextWord());
 			}
 		});
 	}
